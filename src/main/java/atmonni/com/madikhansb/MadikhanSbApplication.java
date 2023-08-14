b726b14e029a0c06a7b20d8d3a6903b79d28e959
JAOUAD OUXHAIB s306971 <s306971@studenti.polito.it>
2023-08-14 12:24:02 +0200

    Initial commit

---
+++ src/main/java/atmonni/com/madikhansb/MadikhanSbApplication.java
@@ -0,0 +1,13 @@
+package atmonni.com.madikhansb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadikhanSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadikhanSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/atmonni/com/madikhansb/MadikhanSbApplicationTests.java
@@ -0,0 +1,13 @@
+package atmonni.com.madikhansb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadikhanSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
